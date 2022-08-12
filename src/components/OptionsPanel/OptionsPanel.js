bd54586eec77f763c78339b99686e4767312ff28
Walid Housni <walid.housni.30@gmail.com>
2022-08-12 22:54:41 +0100

    Completed module

--- src/components/OptionsPanel/OptionsPanel.js
+++ src/components/OptionsPanel/OptionsPanel.js
@@ -5,7 +5,7 @@ import TileSelector from '../TileSelector'
 
 const OptionsPanel = (props) => (
   <div>
-    <TileSelector handleTileNumChange={props.handleNumTileChange} numTiles={props.numTiles} />
+    <TileSelector handleNumTileChange={props.handleNumTileChange} numTiles={props.numTiles} />
     <Button playing={props.playing} startGame={props.startGame} />
   </div>
 )
