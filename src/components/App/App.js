42bb57da860604fe2256c4dee08d1bd3ff4ba6f8
Walid Housni <walid.housni.30@gmail.com>
2022-08-12 12:53:52 +0100

    Completed module

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import {createTiles} from '../../misc/utils';
+import {createTiles, indexOfSelected} from '../../misc/utils';
 
 import './App.css';
 
@@ -23,8 +23,41 @@ class App extends Component{
       playing: true,
       previousTileIndex: null,
       toBeCleared: null,
-      tiles: createTiles(state.numTiles)
+      tiles: createTiles(state.numTiles, this.handleTileClicked)
     }))
+  }
+
+  handleTileClicked = (id, color)  => {
+     this.setState((state) => {
+       const tiles = state.tiles;
+       let toBeCleared = state.toBeCleared;
+       const selectedTileIndex = indexOfSelected(tiles, id, color);
+       let previousTileIndex =  state.previousTileIndex;
+
+       if (toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false;
+        tiles[toBeCleared[1]].selected = false
+        toBeCleared = null
+       }
+
+       tiles[selectedTileIndex].selected = true
+
+       if (previousTileIndex !== null) {
+        const previousTile = tiles[previousTileIndex]
+        const selectedTile = tiles[selectedTileIndex]
+
+        if (previousTile.id !== selectedTile.id !== previousTile.color == color) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex]
+          previousTileIndex = null
+        }
+       } else {
+        previousTileIndex = selectedTileIndex
+       }
+     })
   }
 
   render() {
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -6,7 +6,7 @@ const Tile = (props) => {
   const dynamicColor = props.selected || props.matched ? { backgroundColor: props.color } : null
 
   return (
-    <div className='Tile' style={dynamicColor}>
+    <div className='Tile' onClick={() => {props.handleTileClicked(props.id, props.color)}} style={dynamicColor}>
        { props.selected || props.matched ? <props.svg /> : null }
     </div>
   )
