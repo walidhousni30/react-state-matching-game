e3968eb709b9247ce770b39c4372463a7758153e
Walid Housni <walid.housni.30@gmail.com>
2022-08-12 23:05:41 +0100

    Completed module

--- src/hooks.js
+++ src/hooks.js
@@ -1,23 +1,21 @@
-import { useRef, useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
+const useHover = () => {
+  const ref = useRef()
+  const [hovered, setHovered] = useState(false)
 
-const useHover = () =>{
-   const ref = useRef();
-   const [ hovered, setHovered ] = useState(false);
+  const enter = () => setHovered(true)
+  const leave = () => setHovered(false)
 
-   const enter = () => setHovered(true)
-   const leave = () => setHovered(false)
+  useEffect(() => {
+    const refCopy = ref
+    refCopy.current.addEventListener('mouseenter', enter)
+    refCopy.current.addEventListener('mouseleave', leave)
+    return () => {
+      refCopy.current.removeEventListener('mouseenter', enter)
+      refCopy.current.removeEventListener('mouseleave', leave)
+    } 
+  })
+  return [ref, hovered]
+}
 
-   useEffect(() => {
-      const refCopy = ref
-      refCopy.current.addEventListener('mouseenter', enter);
-      refCopy.current.addEventListener('mouseleave', leave);
-      return () => {
-        refCopy.current.removeEventListener('mouseneter', enter)
-        refCopy.current.removeEventListener('mouseleave', leave)
-      }
-    })
-
-   return [ref, hovered]
- }
-
-export default useHover;
+export default useHover
