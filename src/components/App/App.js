97b8f72cfe7cf358863f53e1c3f2637fd47282ad
Walid Housni <walid.housni.30@gmail.com>
2022-08-12 01:28:13 +0100

    Completed module

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -33,7 +33,7 @@ class App extends Component{
       <header className="App-header">
         Turbo-Matcher
       </header>
-        <OptionsPanel playing={this.state.playing}  numTiles={this.state.numTiles} />
+        <OptionsPanel playing={this.state.playing}  numTiles={this.state.numTiles} startGame={this.startGame} />
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles}  />
     </div>
   );
