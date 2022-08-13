87f31190334eabc1efc6f48ab5886af9ba126f05
Walid Housni <walid.housni.30@gmail.com>
2022-08-13 03:20:51 +0100

    Completed module

--- refactor/components/Button/Button.js
+++ refactor/components/Button/Button.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Button.css';
+import GameContext from '../../GameContext'
 
-const Button = (props) => ( <button onClick={props.startGame}>{props.playing ? 'reset' : 'start'}</button> )
+const Button = (props) => ( 
+  <GameContext.Consumer>
+    {
+      ({playing, startGame}) =>
+        <button onClick={startGame}>
+          {playing ? 'reset' : 'start'}
+        </button>
+    }
+  </GameContext.Consumer>
+)
 
 export default Button
